fb51b6bc217d8dc7790d089a40db0521ebf26826
djoseneto <dev.joseneto@gmail.com>
2022-09-16 12:27:29 -0400

    feat: create navbar and section hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
-export const App = () => {
-  return <div />;
-};
+import styles from "./style";
+import { Navbar, Hero } from "./components";
+
+function App() {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>testes</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billind from "./Billing";
+import Business from "./Business";
+import Hero from "./Hero";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+
+export {
+  Navbar,
+  Billind,
+  Business,
+  Hero,
+  CardDeal,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+};
